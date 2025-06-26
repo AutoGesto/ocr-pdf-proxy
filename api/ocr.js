dffd59b74ac98b015855dc426b1722f680d7cebe
AutoGesto <automatizacion.gestoria@gmail.com>
2025-06-26 04:17:25 +0200

    Update ocr.js

--- api/ocr.js
+++ api/ocr.js
@@ -1,6 +1,5 @@
-import Tesseract from 'tesseract.js';
 import { buffer } from 'micro';
-import { createWorker } from 'tesseract.js';
+import pdfParse from 'pdf-parse';
 
 export const config = {
   api: {
@@ -11,17 +10,10 @@ export const config = {
 export default async function handler(req, res) {
   try {
     const buf = await buffer(req);
-    const worker = await createWorker(['eng', 'spa']);
-
-    const {
-      data: { text },
-    } = await worker.recognize(buf);
-
-    await worker.terminate();
-
-    res.status(200).json({ text });
+    const data = await pdfParse(buf);
+    res.status(200).json({ text: data.text });
   } catch (error) {
-    console.error('Error en OCR:', error);
+    console.error('Error al procesar el PDF:', error);
     res.status(500).json({ error: 'Error al procesar el archivo PDF.' });
   }
 }
